7fc25bc72f5022e39c2dce67d8d18a5778ab2ce6
“Nik <nikolaspreyes@gmail.com>
2025-03-18 16:33:04 -0700

    feature: add paint tile feature on mouse click

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-function generateTiles(...numOfTiles) {
+// rest parameter allows for variable skecth area size (square, portrait, landscape)
+const generateTiles = (...numOfTiles) => {
   const sketchArea = document.querySelector(".sketch-area");
   const rowsFrag = document.createDocumentFragment();
   for (let i = 0; i < numOfTiles[0]; i++) {
@@ -12,5 +13,22 @@ function generateTiles(...numOfTiles) {
     rowsFrag.appendChild(row);
   }
   sketchArea.appendChild(rowsFrag);
-}
+};
 generateTiles(8, 8);
+
+const paintTile = (e) => {
+  const tile = e.target;
+  const computedBgColor =
+    getComputedStyle(tile).getPropertyValue("background-color");
+  const colorValues = computedBgColor.match(/[\d.]+/g);
+  let alpha = colorValues.length === 3 ? 1 : parseFloat(colorValues[3]);
+
+  if (!alpha > 0) {
+    alpha = 0.5;
+  } else {
+    alpha += 0.1;
+  }
+  tile.style.backgroundColor = `rgba(${0}, ${0}, ${0}, ${alpha})`;
+};
+const sketchArea = document.querySelector(".sketch-area");
+sketchArea.addEventListener("click", paintTile);
