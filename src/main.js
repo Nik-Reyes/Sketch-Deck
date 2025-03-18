a542dce722202445a63a46b46194e22110c4a07b
“Nik <nikolaspreyes@gmail.com>
2025-03-17 20:31:57 -0700

    fix: use fragments to optimize DOM manipulation
    in contrast to appending row to sketch area (up to n times), append rows to a fragment, and then append the fragment
    (complete grid) to the sketch area at the end. Result: takes n DOM manipulations down to just 1, eliminating unecessary
    reflows

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 function generateTiles(...numOfTiles) {
   const sketchArea = document.querySelector(".sketch-area");
+  const rowsFrag = document.createDocumentFragment();
   for (let i = 0; i < numOfTiles[0]; i++) {
     const row = document.createElement("div");
     row.classList.add("tile-row");
-    for (j = 0; j < numOfTiles[0]; j++) {
+    for (let j = 0; j < numOfTiles[0]; j++) {
       const tile = document.createElement("div");
       tile.classList.add("tile");
       row.appendChild(tile);
-      sketchArea.appendChild(row);
     }
+    rowsFrag.appendChild(row);
   }
+  sketchArea.appendChild(rowsFrag);
 }
-
-generateTiles(64, 5);
+generateTiles(8, 8);
