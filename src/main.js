086566903b564cd898876b25683d10ce9e1b7549
“Nik <nikolaspreyes@gmail.com>
2025-03-20 12:26:24 -0700

    care-package: add feature, style, and refactor main.js
    refactor: move all arrow functions in event listeners to their own functions and move all event listeners, function
    calls, variable declarations and initializations to bottom of main.js
    feature: add button to toggle tile borders, toggle center grid points; additionally append center div to tile on grid
    creation
    style: add .border class to make toggling border easy; add .hidden class to toggle center tacks; add .center-tile to
    style center tacks

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// rest parameter allows for variable skecth area size (square, portrait, landscape)
-const initialGridSize = 16;
 const generateTiles = (...numOfTiles) => {
   const sketchArea = document.querySelector(".sketch-area");
   const grid = document.createDocumentFragment();
@@ -8,17 +6,18 @@ const generateTiles = (...numOfTiles) => {
 
   for (let i = 0; i < numOfTiles[0]; i++) {
     const tile = document.createElement("div");
-    tile.className = "tile";
+    tile.classList.add("tile", "border");
+    const center = document.createElement("div");
+    center.className = "center-tile";
+    tile.appendChild(center);
     row.appendChild(tile);
   }
 
   for (let i = 0; i < numOfTiles[0]; i++) {
     grid.appendChild(row.cloneNode(true));
   }
-
   sketchArea.appendChild(grid);
 };
-generateTiles((x = initialGridSize), (y = initialGridSize));
 
 const paintTile = (e) => {
   //known bug: right click still allows painting
@@ -27,18 +26,69 @@ const paintTile = (e) => {
     const computedBgColor =
       getComputedStyle(tile).getPropertyValue("background-color");
     const colorValues = computedBgColor.match(/[\d.]+/g);
-    let alpha = colorValues.length === 3 ? 1 : parseFloat(colorValues[3]);
+    let alpha = colorValues.length === 3 ? 1.0 : parseFloat(colorValues[3]);
 
-    if (!alpha > 0) {
-      alpha = 0.5;
-    } else {
-      alpha += 0.1;
+    if (alpha !== 1.0) {
+      if (!alpha > 0) {
+        alpha = 0.5;
+      } else {
+        alpha += 0.1;
+      }
+      tile.style.backgroundColor = `rgba(${0}, ${0}, ${0}, ${alpha})`;
     }
-    tile.style.backgroundColor = `rgba(${0}, ${0}, ${0}, ${alpha})`;
   }
 };
 
+function clearGrid() {
+  const tiles = document.querySelectorAll(".tile");
+  const tileArray = Array.from(tiles).filter((tile) => {
+    return (
+      getComputedStyle(tile).getPropertyValue("background-color") !==
+      "rgba(0, 0, 0, 0)"
+    );
+  });
+  tileArray.forEach(
+    (tile) => (tile.style.backgroundColor = `rgba(0, 0, 0, 0)`)
+  );
+}
+
+const resizeGrid = (e) => {
+  e.preventDefault();
+  if (!input.validity.valid) {
+    if (input.validity.valueMissing) {
+      console.log("Warning!\nMust input a value");
+    } else if (input.validity.patternMismatch) {
+      console.log("Please enter only digits [1-9]");
+    }
+    return;
+  }
+  const tileList = document.querySelectorAll(".tile-row");
+  console.log(tileList);
+  tileList.forEach((row) => row.remove());
+  generateTiles(input.value, input.value);
+};
+
+function toggleCenterTacks() {
+  const centers = document.querySelectorAll(".center-tile");
+  console.log(centers);
+  centers.forEach((center) => center.classList.toggle("hidden"));
+}
+
+const toggleBorders = () => {
+  const tiles = document.querySelectorAll(".tile");
+  console.log("hi");
+  tiles.forEach((tile) => tile.classList.toggle("border"));
+};
+
+generateTiles((x = 16), (y = 16));
+
+const resetButton = document.querySelector("#clear-grid");
 const sketchArea = document.querySelector(".sketch-area");
+const borderButton = document.querySelector("#toggle-borders");
+const centerButton = document.querySelector("#toggle-centers");
+const form = document.querySelector("#grid-size");
+const input = document.querySelector("#grid-dimensions");
+
 sketchArea.addEventListener("mousedown", () => {
   sketchArea.addEventListener("mouseover", paintTile);
 });
@@ -53,32 +103,7 @@ sketchArea.addEventListener("mouseleave", () => {
   sketchArea.removeEventListener("mouseover", paintTile);
 });
 
-const resetButton = document.querySelector("#clear-grid");
-resetButton.addEventListener("click", () => {
-  tiles = Array.from(document.querySelectorAll(".tile")).filter((tile) => {
-    return (
-      getComputedStyle(tile).getPropertyValue("background-color") !==
-      "rgba(0, 0, 0, 0)"
-    );
-  });
-  tiles.forEach((tile) => (tile.style.backgroundColor = `rgba(0, 0, 0, 0)`));
-});
-
-const form = document.querySelector("#grid-size");
-const input = document.querySelector("#grid-dimensions");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (!input.validity.valid) {
-    if (input.validity.valueMissing) {
-      console.log("Warning!\nMust input a value");
-    } else if (input.validity.patternMismatch) {
-      console.log("Please enter only digits [1-9]");
-    }
-    return;
-  }
-  const tileList = document.querySelectorAll(".tile-row");
-  console.log(tileList);
-  tileList.forEach((row) => row.remove());
-  generateTiles(input.value, input.value);
-});
+form.addEventListener("submit", resizeGrid);
+borderButton.addEventListener("click", toggleBorders);
+resetButton.addEventListener("click", clearGrid);
+centerButton.addEventListener("click", toggleCenterTacks);
