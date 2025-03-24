b730e7034959bc27f619ed39fab582dae060f7c1
“Nik <nikolaspreyes@gmail.com>
2025-03-24 15:55:27 -0700

    feature: add dynamic input notifications

--- src/main.js
+++ src/main.js
@@ -81,19 +81,114 @@ function clearGrid() {
   );
 }
 
-const resizeGrid = (e) => {
-  e.preventDefault();
-  if (!input.validity.valid) {
-    if (input.validity.valueMissing) {
-      console.log("Warning!\nMust input a value");
-    } else if (input.validity.patternMismatch) {
-      console.log("Please enter only digits [1-9]");
-    }
-    return;
-  }
+function resize() {
   const tileList = document.querySelectorAll(".tile-row");
   tileList.forEach((row) => row.remove());
   generateTiles(input.value, input.value);
+}
+
+function createModal(message, buttons) {
+  //get necessary elements to create a new modal
+  //1.Need access to the p tag
+  //2. need to implant the buttons into the button-wrapper
+  //3. need to show/unhide the warning-modal
+
+  const modal = document.querySelector(".warning-modal");
+  const modalMessage = modal.querySelector("p");
+  const buttonWrapper = modal.querySelector(".warning-button-wrapper");
+
+  //Create the message
+  modalMessage.textContent = message;
+
+  //Create the buttons
+  const newButtons = [];
+  for (let i = 0; i < buttons.length; i++) {
+    const buttonContainer = document.createElement("div");
+    buttonContainer.className = "warning-buttons";
+
+    const button = document.createElement("button");
+    button.id = buttons[i].id;
+    button.className = "choice-button";
+    button.innerText = buttons[i].text;
+
+    buttonContainer.appendChild(button);
+    newButtons.push(buttonContainer);
+  }
+
+  //clear any previous buttons and replace them with the newly created ones
+  buttonWrapper.replaceChildren(...newButtons);
+  console.log(buttonWrapper);
+
+  //Now that the modal has been created, toggle the hidden class
+  modal.classList.toggle("hidden");
+}
+
+const inputNotification = (e) => {
+  input.toggleAttribute("disabled");
+  e.preventDefault();
+  if (!input.validity.valid) {
+    if (input.validity.valueMissing) {
+      createModal("Warning!\nMust input a value", [{ id: "ok", text: "OK" }]);
+    } else if (input.validity.patternMismatch) {
+      createModal("Please enter only digits [1-9]", [{ id: "ok", text: "OK" }]);
+    }
+    return;
+  }
+
+  if (input.value > 64 && input.value <= 99) {
+    createModal("Ummmmmm, no thats too big of a grid.", [
+      { id: "ok", text: "OK" },
+    ]);
+    return;
+  }
+
+  if (input.value >= 100 && input.value <= 249) {
+    createModal(
+      "Hmmmmm. Ok. You must really want a large grid. Too bad so sad.",
+      [{ id: "ok", text: "OK" }]
+    );
+    return;
+  }
+
+  if (input.value >= 250 && input.value <= 499) {
+    createModal(
+      "Reeeeeaaaaalllllyyyyy??? You really wanna go that big? You sure? You double double sure? I don't think you're sure. Sorry, no big grid for u!",
+      [{ id: "ok", text: "OK" }]
+    );
+    return;
+  }
+
+  if (input.value >= 500 && input.value <= 799) {
+    createModal(
+      "You are out of your mind. This big of a grid would be too large to see an individual tile. No way jose.",
+      [{ id: "ok", text: "OK" }]
+    );
+    return;
+  }
+
+  if (input.value >= 800 && input.value <= 998) {
+    createModal(
+      "You are a bold one! I am almost willing to allow such a large grid. ALMOST! Hah!, better luck next time.",
+      [{ id: "ok", text: "OK" }]
+    );
+    return;
+  }
+
+  if (input.value >= 999) {
+    createModal(
+      "You know what, fine. You can have it. But uhhhh, good luck sketching anything. Let alone even getting your broswer to respond lol.",
+      [{ id: "big-grid", text: "Big grid" }]
+    );
+    return;
+  }
+
+  createModal(
+    "Warning! Resizing the grid will ERASE your drawing! Would you like to proceed?",
+    [
+      { id: "cancel", text: "NO" },
+      { id: "confirm", text: "YES" },
+    ]
+  );
 };
 
 function toggleCenterTacks() {
@@ -123,6 +218,7 @@ const shadeMode = document.querySelector("#shade");
 const blackMode = document.querySelector("#black");
 const rainbowMode = document.querySelector("#rainbow");
 const eraseMode = document.querySelector("#erase");
+const warningModal = document.querySelector(".warning-modal");
 
 sketchArea.addEventListener("mousedown", () => {
   sketchArea.addEventListener("mouseover", paintTile);
@@ -138,15 +234,21 @@ sketchArea.addEventListener("mouseleave", () => {
   sketchArea.removeEventListener("mouseover", paintTile);
 });
 
-form.addEventListener("submit", resizeGrid);
+form.addEventListener("submit", inputNotification);
 borderButton.addEventListener("click", toggleBorders);
 resetButton.addEventListener("click", clearGrid);
 centerButton.addEventListener("click", toggleCenterTacks);
-blackMode.addEventListener("click", () => (brushMode = 0));
-shadeMode.addEventListener("click", () => (brushMode = 1));
-eraseMode.addEventListener("click", () => (brushMode = 3));
-
-// toggle borders WScript
-// const children = Array.from(e.target.closest("#toggle-borders").childNodes);
-//   // const bezels = children.filter((child) => child.className !== "");
-//   // console.log(bezels);
+blackMode.addEventListener("click", (e) => (brushMode = 0));
+shadeMode.addEventListener("click", (e) => (brushMode = 1));
+eraseMode.addEventListener("click", (e) => (brushMode = 3));
+warningModal.addEventListener("click", (e) => {
+  const modalButton = e.target.closest("button");
+  if (!modalButton) {
+    return;
+  }
+  if (modalButton.id === "confirm" || modalButton.id === "big-grid") {
+    resize();
+  }
+  warningModal.classList.toggle("hidden");
+  input.toggleAttribute("disabled");
+});
